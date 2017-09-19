c7dca904ee47fff482fc8b27b524679d3e4d7354
yangxinlin <yangxinlin@58ganji.com>
2017-09-19 16:16:44 +0800

    使用cocospod添加SDWebImage、AFNetWorking

---
+++ UI堆栈架构/Pods/Target Support Files/Pods-UI堆栈架构Tests/Pods-UI堆栈架构Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UI____Tests : NSObject
+@end
+@implementation PodsDummy_Pods_UI____Tests
+@end
---
+++ UI堆栈架构/Pods/Target Support Files/Pods-UI堆栈架构UITests/Pods-UI堆栈架构UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UI____UITests : NSObject
+@end
+@implementation PodsDummy_Pods_UI____UITests
+@end
